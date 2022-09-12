8188d97a6feaa2e15976d4797ba41296c109890f
NichlasMau <Nich5384@stud.kea.dk>
2022-09-12 10:10:43 +0200

    Tilføjet af get/set metoder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         Person p1 = new Person(1.72);
-        Person p2 = new Person();
+        System.out.println(p1.getHøjde());
+        p1.setHøjde(1.79);
+        System.out.println("ny højde " + p1.getHøjde());
     }
+
 }
